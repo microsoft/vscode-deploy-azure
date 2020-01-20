3f828a61bd9b23a12091f9e59dede274ed9c9f7f
Nitin Issac Joy <nijoy@microsoft.com>
2020-01-20 14:59:36 +0530

    Review comments

--- src/configure/clients/github/githubClient.ts
+++ src/configure/clients/github/githubClient.ts
@@ -1,12 +1,9 @@
 import { RestClient } from "../restClient";
 import { UrlBasedRequestPrepareOptions } from 'ms-rest';
-import { SodiumLibHelper } from "../../helper/sodium/SodiumLibHelper";
 import { GitHubProvider } from "../../helper/gitHubHelper";
+import { SodiumLibHelper } from "../../helper/sodium/SodiumLibHelper";
 
-interface GitHubSecretKey {
-    key_id: string;
-    key: string;
-}
+const UserAgent = "deploy-to-azure-vscode";
 
 export class GithubClient {
 
@@ -18,13 +15,21 @@ export class GithubClient {
         this.url = remoteUrl;
     }
 
+    public async createOrUpdateGithubSecret(secretName: string, body: string): Promise<void> {
+        let secretKeyObject: GitHubSecretKey = await this._getGitHubSecretKey();
+        let sodiumObj = new SodiumLibHelper(secretKeyObject.key);
+        let encryptedBytes: Uint8Array = sodiumObj.encrypt(body);
+        let encryptedBytesAsString: string = sodiumObj.convertUint8ArrayToString(encryptedBytes);
+        let encryptedEncodedText = sodiumObj.encodeToBase64(encryptedBytesAsString);
+        await this._setGithubSecret(secretName, secretKeyObject.key_id, encryptedEncodedText);
+    }
     
     private async _getGitHubSecretKey() : Promise<GitHubSecretKey> {
         let request = <UrlBasedRequestPrepareOptions> {
                 url: GitHubProvider.getFormattedGitHubApiUrlBase(this.url) + "/actions/secrets/public-key",
                 method: 'GET',
                 headers: {
-                    "User-Agent": "vscode",
+                    "User-Agent": UserAgent,
                     "Content-Type": "application/json",
                     "Authorization": "Bearer " + this.patToken,
                     "Accept": "*/*" 
@@ -41,7 +46,7 @@ export class GithubClient {
         let request = <UrlBasedRequestPrepareOptions>{
             url: GitHubProvider.getFormattedGitHubApiUrlBase(this.url) + "/actions/secrets/" + secretName,
             headers: {
-                "User-Agent": "vscode",
+                "User-Agent": UserAgent,
                 "Content-Type": "application/json",
                 "Authorization": "Bearer " + this.patToken,
                 "Accept": "*/*"
@@ -56,13 +61,9 @@ export class GithubClient {
         }
         await restClient.sendRequest(request);
     }
+}
 
-    public async createOrUpdateGithubSecret(secretName: string, body: string): Promise<void> {
-        let secretKeyObject: GitHubSecretKey = await this._getGitHubSecretKey();
-        let sodiumObj = new SodiumLibHelper(secretKeyObject.key);
-        let encryptedBytes: Uint8Array = sodiumObj.encrypt(body);
-        let encryptedBytesAsString: string = sodiumObj.convertUint8ArrayToString(encryptedBytes);
-        let encryptedEncodedText = sodiumObj.encodeToBase64(encryptedBytesAsString);
-        await this._setGithubSecret(secretName, secretKeyObject.key_id, encryptedEncodedText);
-    }
+interface GitHubSecretKey {
+    key_id: string;
+    key: string;
 }
