183dacd0b4503c7e2507487ab4c766ed8c0595f5
JA-04 <t-juagar@microsoft.com>
2020-07-16 19:52:31 +0530

    Removed 'before's

--- src/configure/test/suite/GitHubClient.test.ts
+++ src/configure/test/suite/GitHubClient.test.ts
@@ -7,37 +7,61 @@ let org = "Organization";
 let repoName = "Repository";
 let githubClient = new GithubClient("PAT", "repoUrl");
 
+var orgList = [
+    {
+        "login": "SampleOrganizationA",
+        "id": 66720950,
+        "url": "https://api.github.com/orgs/SampleOrganizationA",
+        "repos_url": "https://api.github.com/orgs/SampleOrganizationA/repos"
+    },
+    {
+        "login": "SampleOrganizationB",
+        "id": 67440137,
+        "url": "https://api.github.com/orgs/SampleOrganizationB",
+        "repos_url": "https://api.github.com/orgs/SampleOrganizationB/repos",
+    },
+    {
+        "login": "SampleOrganizationC",
+        "id": 67453753,
+        "url": "https://api.github.com/orgs/SampleOrganizationC",
+        "repos_url": "https://api.github.com/orgs/SampleOrganizationC/repos"
+    }];
+
+var repoCreationResponseOnSuccess = {
+    "id": 278008782,
+    "name": repoName,
+    "private": true,
+    "owner": {
+        "login": org,
+        "id": 67440137
+    },
+    "html_url": "https://github.com/" + org + "/" + repoName,
+    "description": "Repo created from VScode extension 'Deploy to Azure'"
+};
+
+var repoCreationResponseOnFailure = {
+    "message": "Resource protected by organization SAML enforcement. You must grant your PAT access to this organization",
+    "documentation_url": "some url"
+};
+
+var repoCreationBody = {
+    "name": repoName,
+    "description": "Repo created from VScode extension 'Deploy to Azure'",
+    "homepage": "https://github.com",
+    "private": true,
+    "has_issues": true,
+    "has_projects": true,
+    "has_wiki": true
+};
+
 
 describe('# Testing listOrganizations() ', function () {
 
-    before(function () {
-        var orgList = [
-            {
-                "login": "SampleOrganizationA",
-                "id": 66720950,
-                "url": "https://api.github.com/orgs/SampleOrganizationA",
-                "repos_url": "https://api.github.com/orgs/SampleOrganizationA/repos"
-            },
-            {
-                "login": "SampleOrganizationB",
-                "id": 67440137,
-                "url": "https://api.github.com/orgs/SampleOrganizationB",
-                "repos_url": "https://api.github.com/orgs/SampleOrganizationB/repos",
-            },
-            {
-                "login": "SampleOrganizationC",
-                "id": 67453753,
-                "url": "https://api.github.com/orgs/SampleOrganizationC",
-                "repos_url": "https://api.github.com/orgs/SampleOrganizationC/repos"
-            }];
-
-        nock('https://api.github.com')
-            .get('/user/orgs')
-            .reply(200, orgList);
-    });
-
     context('User is a member of organizations(s)', function () {
         it('should return a list of organizations', function (done) {
+            nock('https://api.github.com')
+                .get('/user/orgs')
+                .reply(200, orgList);
             githubClient.listOrganizations(true).then((orgs) => {
                 expect(Array.isArray(orgs)).to.equal(true);
                 expect(orgs.length).to.equal(3);
@@ -50,15 +74,12 @@ describe('# Testing listOrganizations() ', function () {
             });
         });
     });
-    before(function () {
-        var response = [];
-        nock('https://api.github.com')
-            .get('/user/orgs')
-            .reply(200, response);
-    });
 
     context('User is not a member of any organization', function () {
         it('Should return an empty list of organization', function (done) {
+            nock('https://api.github.com')
+                .get('/user/orgs')
+                .reply(200, []);
             githubClient.listOrganizations(true).then((orgs) => {
                 expect(Array.isArray(orgs)).to.equal(true);
                 expect(orgs.length).to.equal(0);
@@ -70,35 +91,11 @@ describe('# Testing listOrganizations() ', function () {
 
 describe('# Testing createGitHubRepo()', function () {
 
-    before(function () {
-        var response = {
-            "id": 278008782,
-            "name": repoName,
-            "private": true,
-            "owner": {
-                "login": org,
-                "id": 67440137
-            },
-            "html_url": "https://github.com/" + org + "/" + repoName,
-            "description": "Repo created from VScode extension 'Deploy to Azure'"
-        };
-        var body = {
-            "name": repoName,
-            "description": "Repo created from VScode extension 'Deploy to Azure'",
-            "homepage": "https://github.com",
-            "private": true,
-            "has_issues": true,
-            "has_projects": true,
-            "has_wiki": true
-        };
-
-        nock('https://api.github.com')
-            .post('/orgs/' + org + '/repos', body)
-            .reply(200, response);
-    });
-
     context('Given repository name is unique', function () {
         it('Should create a new repository', function (done) {
+            nock('https://api.github.com')
+                .post('/orgs/' + org + '/repos', repoCreationBody)
+                .reply(200, repoCreationResponseOnSuccess);
             githubClient.createGithubRepo(org, repoName).then((repo) => {
                 expect(repo).to.not.deep.equal(null);
                 done();
@@ -106,26 +103,11 @@ describe('# Testing createGitHubRepo()', function () {
         });
     });
 
-    before(function () {
-        var response = {
-            "message": "Repository creation failed.",
-            "errors": [
-                {
-                    "resource": "Repository",
-                    "code": "custom",
-                    "field": "name",
-                    "message": "name already exists on this account"
-                }
-            ],
-            "documentation_url": "https://developer.github.com/v3/repos/#create"
-        };
-        nock('https://api.github.com')
-            .post('/orgs/' + org + '/repos')
-            .reply(422, response);
-    });
-
     context('Repository creation fails due to existence of repository of same name within the organization', function () {
         it('Should return null', function (done) {
+            nock('https://api.github.com')
+                .post('/orgs/' + org + '/repos')
+                .reply(422, repoCreationResponseOnFailure);
             githubClient.createGithubRepo(org, repoName).then((repo) => {
                 expect(repo).to.equal(null);
                 done();
@@ -133,19 +115,11 @@ describe('# Testing createGitHubRepo()', function () {
         });
     });
 
-    before(function () {
-        var response = {
-            "message": "Resource protected by organization SAML enforcement. You must grant your PAT access to this organization",
-            "documentation_url": "some url"
-        };
-        nock('https://api.github.com')
-            .post('/orgs/' + org + '/repos')
-            .reply(400, response);
-    });
-
-
     context('Repository creation fails due to some other reason', function () {
         it('Should throw error', function (done) {
+            nock('https://api.github.com')
+                .post('/orgs/' + org + '/repos')
+                .reply(400, repoCreationResponseOnFailure);
             githubClient.createGithubRepo(org, repoName).then(function () {
                 done();
             }).catch(err => {
--- src/configure/test/suite/commonHelper.test.ts
+++ src/configure/test/suite/commonHelper.test.ts
@@ -1,15 +1,12 @@
 import { GithubClient } from "../../clients/github/githubClient";
 import { generateGitHubRepository } from "../../helper/commonHelper";
 
-const uuid = require('uuid/v4');
-
 var expect = require('chai').expect;
 var nock = require('nock');
 
 let org = "Organization";
 let localPath = "c:/directory/Repository";
 let githubClient = new GithubClient("PAT", localPath);
-let uniqueId = uuid().substr(0, 5);
 let repoName = "Repository";
 
 var responseOnFailure = {
@@ -24,8 +21,20 @@ var responseOnFailure = {
     ],
 };
 
+var responseOnSuccess = {
+    "id": 278008782,
+    "name": "Some repo name",
+    "private": true,
+    "owner": {
+        "login": org,
+        "id": 67440137
+    },
+    "html_url": "https://github.com/" + org + "/" + repoName,
+    "description": "Repo created from VScode extension 'Deploy to Azure'"
+};
+
 var body1 = {
-    "name": repoName,
+    "name": "Repository",
     "description": "Repo created from VScode extension 'Deploy to Azure'",
     "homepage": "https://github.com",
     "private": true,
@@ -35,7 +44,7 @@ var body1 = {
 };
 
 var body2 = {
-    "name": repoName + "_" + org,
+    "name": "Repository_Organization",
     "description": "Repo created from VScode extension 'Deploy to Azure'",
     "homepage": "https://github.com",
     "private": true,
@@ -43,7 +52,6 @@ var body2 = {
     "has_projects": true,
     "has_wiki": true
 };
-
 
 var body3 = {
     "name": /Repository_Organization_[a-zA-Z0-9]*/,
@@ -57,55 +65,25 @@ var body3 = {
 
 describe('# Testing generateGithubRepository()', function () {
 
-    before(function () {
-        var responseOnSuccess = {
-            "id": 278008782,
-            "name": repoName,
-            "private": true,
-            "owner": {
-                "login": org,
-                "id": 67440137
-            },
-            "html_url": "https://github.com/" + org + "/" + repoName,
-            "description": "Repo created from VScode extension 'Deploy to Azure'"
-        };
-
-        nock('https://api.github.com')
-            .post('/orgs/' + org + '/repos', body1)
-            .reply(200, responseOnSuccess);
-    });
-
     context('Local repository name is unique inside selected organization', function () {
         it('Should create a new repository with the same name as local repository name', function (done) {
+            nock('https://api.github.com')
+                .post('/orgs/' + org + '/repos', body1)
+                .reply(200, responseOnSuccess);
             generateGitHubRepository(org, localPath, githubClient).then((repo) => {
                 expect(repo).to.not.deep.equal(null);
                 done();
             });
         });
-    });
-
-    before(function () {
-        var responseOnSuccess = {
-            "id": 278008782,
-            "name": repoName + "_" + org,
-            "private": true,
-            "owner": {
-                "login": org,
-                "id": 67440137
-            },
-            "html_url": "https://github.com/" + org + "/" + repoName + "_" + org,
-            "description": "Repo created from VScode extension 'Deploy to Azure'"
-        };
-
-        nock('https://api.github.com')
-            .post('/orgs/' + org + '/repos', body1)
-            .reply(422, responseOnFailure)
-            .post('/orgs/' + org + '/repos', body2)
-            .reply(200, responseOnSuccess);
     });
 
     context('Selected organization contains a repository with same name as local repository', function () {
         it('Should create a new repository with the name = "local name + org name"', function (done) {
+            nock('https://api.github.com')
+                .post('/orgs/' + org + '/repos', body1)
+                .reply(422, responseOnFailure)
+                .post('/orgs/' + org + '/repos', body2)
+                .reply(200, responseOnSuccess);
             generateGitHubRepository(org, localPath, githubClient).then((repo) => {
                 expect(repo).to.not.deep.equal(null);
                 done();
@@ -113,29 +91,15 @@ describe('# Testing generateGithubRepository()', function () {
         });
     });
 
-    before(function () {
-        var responseOnSuccess = {
-            "id": 278008782,
-            "name": repoName + "_" + org + "_" + "3ed45",
-            "private": true,
-            "owner": {
-                "login": org,
-                "id": 67440137
-            },
-            "html_url": "https://github.com/" + org + "/" + repoName + "_" + org + "_" + uniqueId,
-            "description": "Repo created from VScode extension 'Deploy to Azure'"
-        };
-        nock('https://api.github.com')
-            .post('/orgs/' + org + '/repos', body1)
-            .reply(422, responseOnFailure)
-            .post('/orgs/' + org + '/repos', body2)
-            .reply(422, responseOnFailure)
-            .post('/orgs/' + org + '/repos', body3)
-            .reply(200, responseOnSuccess);
-    });
-
     context('Selected organization contains a repository with same name as local repository and another repository with the name - "Local repository name + Organization Name"', function () {
         it('Should create a new repository with the name = "local name + org name + uuid"', function (done) {
+            nock('https://api.github.com')
+                .post('/orgs/' + org + '/repos', body1)
+                .reply(422, responseOnFailure)
+                .post('/orgs/' + org + '/repos', body2)
+                .reply(422, responseOnFailure)
+                .post('/orgs/' + org + '/repos', body3)
+                .reply(200, responseOnSuccess);
             generateGitHubRepository(org, localPath, githubClient).then((repo) => {
                 expect(repo).to.not.deep.equal(null);
                 done();
