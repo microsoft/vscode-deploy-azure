928d27fb8abd18857f1e596658bcc5ca2d5dfaa8
Vineet <vimimrot@microsoft.com>
2020-08-11 11:25:10 +0530

    minor fix

--- src/configure/helper/azureSessionHelper.ts
+++ src/configure/helper/azureSessionHelper.ts
@@ -1,6 +1,7 @@
 import { SubscriptionModels } from "azure-arm-resource";
 import { isNullOrUndefined } from "util";
 import { AzureSession, extensionVariables } from "../model/models";
+import { Messages } from "../resources/messages";
 
 export function getSubscriptionSession(subscriptionId: string): AzureSession {
     let currentSubscription: { session: AzureSession, subscription: SubscriptionModels.Subscription } = extensionVariables.azureAccountExtensionApi.subscriptions
@@ -17,10 +18,15 @@ export function getSubscriptionSession(subscriptionId: string): AzureSession {
 
 export async function getAzureSession(): Promise<AzureSession> {
     let currentSubscription = extensionVariables.azureAccountExtensionApi.subscriptions[0];
-    while (isNullOrUndefined(currentSubscription)) {
+    let count = 0;
+    while (isNullOrUndefined(currentSubscription) && count++ < 5) {
         // tslint:disable-next-line: no-string-based-set-timeout
         await new Promise((resolve) => setTimeout(resolve, 1000));
         currentSubscription = extensionVariables.azureAccountExtensionApi.subscriptions[0];
+    }
+
+    if (isNullOrUndefined(currentSubscription)) {
+        throw new Error(Messages.AzureLoginError);
     }
 
     return currentSubscription.session;
