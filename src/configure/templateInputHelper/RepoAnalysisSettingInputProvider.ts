bd28b2948e13becb4d17d2f702805ba1eef6c2c8
Anurag Chauhan <44864882+anuragc617@users.noreply.github.com>
2020-09-07 01:06:43 +0530

    Adding support for array repo analysis setting

--- src/configure/templateInputHelper/RepoAnalysisSettingInputProvider.ts
+++ src/configure/templateInputHelper/RepoAnalysisSettingInputProvider.ts
@@ -1,6 +1,7 @@
 import { ApplicationSettings } from "azureintegration-repoanalysis-client-internal";
 import { ControlProvider } from "../helper/controlProvider";
 import { telemetryHelper } from "../helper/telemetryHelper";
+import { InputDataType } from "../model/Contracts";
 import { TracePoints } from "../resources/tracePoints";
 import { InputControl } from "./InputControl";
 
@@ -24,7 +25,8 @@ export class RepoAnalysisSettingInputProvider {
     public async setInputControlValueFromRepoAnalysisResult(inputControl: InputControl): Promise<void> {
         let repoAnalysisSettingKey = inputControl.getPropertyValue(this.repoAnalysisSettingKey);
         if (this._selectedRepoAnalysisSettingIndex !== -1) {
-            if (!this._repoAnalysisSettings[this._selectedRepoAnalysisSettingIndex].settings[repoAnalysisSettingKey]) {
+            let value = this._repoAnalysisSettings[this._selectedRepoAnalysisSettingIndex].settings[repoAnalysisSettingKey];
+            if (!value || (Array.isArray(value) && value.length === 0)) {
                 let error = new Error(`RepostioryAnalysisSetting doesn't contain ${repoAnalysisSettingKey} for input ${inputControl.getInputControlId()}`);
                 telemetryHelper.logError(Layer, TracePoints.SetInputControlValueFromRepoAnalysisResult, error);
                 if (inputControl.getInputDescriptor().defaultValue) {
@@ -38,7 +40,18 @@ export class RepoAnalysisSettingInputProvider {
                     inputControl.setValue(value);
                 }
             } else {
-                inputControl.setValue(this._repoAnalysisSettings[this._selectedRepoAnalysisSettingIndex].settings[repoAnalysisSettingKey]);
+                if (Array.isArray(value)) {
+                    let selectedValue = value[0];
+                    if (value.length > 1) {
+                        let possibleValues = value.map((value) => ({ label: value, data: value }));
+
+                        selectedValue = (await new ControlProvider().showQuickPick(repoAnalysisSettingKey, possibleValues, {
+                            placeHolder: inputControl.getInputDescriptor().name,
+                        })).data;
+                    }
+                    value = selectedValue;
+                }
+                inputControl.setValue(value);
             }
         } else {
             let settingIndexMap: Map<string, number[]> = new Map();
@@ -46,10 +59,15 @@ export class RepoAnalysisSettingInputProvider {
                 if (!analysisSetting.settings[repoAnalysisSettingKey]) {
                     return;
                 }
-                if (settingIndexMap.has(analysisSetting.settings[repoAnalysisSettingKey])) {
-                    settingIndexMap.get(analysisSetting.settings[repoAnalysisSettingKey]).push(index);
+                let keyValue = analysisSetting.settings[repoAnalysisSettingKey];
+                if (Array.isArray(keyValue)) {
+                    keyValue = keyValue.toString();
+                }
+
+                if (settingIndexMap.has(keyValue)) {
+                    settingIndexMap.get(keyValue).push(index);
                 } else {
-                    settingIndexMap.set(analysisSetting.settings[repoAnalysisSettingKey], [index]);
+                    settingIndexMap.set(keyValue, [index]);
                 }
             });
             if (settingIndexMap.size === 0) {
@@ -60,8 +78,7 @@ export class RepoAnalysisSettingInputProvider {
                     validateInput: value => inputControl.triggerControlValueValidations(value)
                 });
                 inputControl.setValue(value);
-            }
-            else {
+            } else {
                 let possibleValues = Array.from(settingIndexMap.keys()).map((value) => ({ label: value, data: value }));
                 let selectedValue: { label: string, data: any };
 
@@ -71,10 +88,16 @@ export class RepoAnalysisSettingInputProvider {
                 else {
                     selectedValue = await new ControlProvider().showQuickPick(repoAnalysisSettingKey, possibleValues, { placeHolder: inputControl.getInputDescriptor().name });
                 }
-
                 if (settingIndexMap.get(selectedValue.data).length === 1) {
                     this._selectedRepoAnalysisSettingIndex = settingIndexMap.get(selectedValue.data)[0];
                 }
+
+                let repoAnalysisValue = this._repoAnalysisSettings[settingIndexMap.get(selectedValue.data)[0]].settings[repoAnalysisSettingKey];
+                if (inputControl.getInputDataType() === InputDataType.String && Array.isArray(repoAnalysisValue)) {
+                    possibleValues = repoAnalysisValue.map((value) => ({ label: value, data: value }));
+                    selectedValue = await new ControlProvider().showQuickPick(repoAnalysisSettingKey, possibleValues, { placeHolder: inputControl.getInputDescriptor().name });
+                }
+
                 inputControl.setValue(selectedValue.data);
             }
         }
