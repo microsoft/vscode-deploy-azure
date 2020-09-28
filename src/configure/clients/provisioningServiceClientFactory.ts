04f9c9f040122bd3bd366ea2eda922d4699feb73
ddivanshu <ddivanshu@github.com>
2020-09-29 02:13:22 +0530

    remove duplicate code

--- src/configure/clients/provisioningServiceClientFactory.ts
+++ src/configure/clients/provisioningServiceClientFactory.ts
@@ -8,8 +8,8 @@ export class ProvisioningServiceClientFactory {
         if (!!this.client) {
             return this.client;
         }
-        const defaultHeaders: { [propertyName: string]: string } = { "Content-Type": "application/json" }; const defaultHeaders: { [propertyName: string]: string } = { "Content-Type": "application/json" };
-        const serviceDefinition = await RemoteServiceUrlHelper.getProvisioningServiceDefinition(); const serviceDefinition = await RemoteServiceUrlHelper.getProvisioningServiceDefinition();
+        const defaultHeaders: { [propertyName: string]: string } = { "Content-Type": "application/json" };
+        const serviceDefinition = await RemoteServiceUrlHelper.getProvisioningServiceDefinition();
         if (serviceDefinition.serviceFramework === ServiceFramework.Vssf) {
             defaultHeaders["X-GITHUB-TOKEN"] = "token " + githubPatToken;
             serviceDefinition.serviceFramework = ServiceFramework.Moda;
