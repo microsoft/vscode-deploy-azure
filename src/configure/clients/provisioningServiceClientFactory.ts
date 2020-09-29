8de31b942a6aa0df42a779400fb35de7e198f154
ddivanshu <36132271+ddivanshu@users.noreply.github.com>
2020-09-29 19:19:15 +0530

    Users/ddivanshu/node provisiong service (#169)
    * prov service
    
    * Revert "prov service"
    
    This reverts commit 31ef6854df2b70a6f6f01efe90df58596db9c64c.
    
    * Revert "Revert "prov service""
    
    This reverts commit b250c7225464404e56fda4a1f3144ca649657369.
    
    * update api
    
    * remove local url
    
    * remove local url
    
    * remove duplicate code
    
    * remove duplicate code
    
    * remove duplicate code

--- src/configure/clients/provisioningServiceClientFactory.ts
+++ src/configure/clients/provisioningServiceClientFactory.ts
@@ -4,12 +4,11 @@ import { IProvisioningServiceClient } from "./IProvisioningServiceClient";
 import { ProvisioningServiceClient } from "./ProvisioningServiceClient";
 
 export class ProvisioningServiceClientFactory {
-    public static async getClient(githubPatToken: string, credentials?: ServiceClientCredentials ): Promise<IProvisioningServiceClient> {
+    public static async getClient(githubPatToken: string, credentials?: ServiceClientCredentials): Promise<IProvisioningServiceClient> {
         if (!!this.client) {
             return this.client;
         }
-
-        const defaultHeaders: { [propertyName: string]: string } =   { "Content-Type": "application/json" };
+        const defaultHeaders: { [propertyName: string]: string } = { "Content-Type": "application/json" };
         const serviceDefinition = await RemoteServiceUrlHelper.getProvisioningServiceDefinition();
         if (serviceDefinition.serviceFramework === ServiceFramework.Vssf) {
             defaultHeaders["X-GITHUB-TOKEN"] = "token " + githubPatToken;
