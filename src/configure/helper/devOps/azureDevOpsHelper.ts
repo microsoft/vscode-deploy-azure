b38fd2d1076ca2bdf69f25ce8370f6a586c34c38
Kanika Pasrija <kanikapasrija@Kanikas-MacBook-Pro.local>
2020-12-04 20:12:21 +0530

    modifying yaml path

--- src/configure/helper/devOps/azureDevOpsHelper.ts
+++ src/configure/helper/devOps/azureDevOpsHelper.ts
@@ -117,7 +117,7 @@ export class AzureDevOpsHelper {
             },
             process: {
                 type: 2,
-                yamlFileName: path.join(inputs.pipelineConfiguration.workingDirectory, path.basename(inputs.pipelineConfiguration.filePath))
+                yamlFileName: path.basename(inputs.pipelineConfiguration.filePath)
             },
             queue: {
                 id: queueId // Default queue Hosted VS 2017. This value is overriden by queue specified in YAML
